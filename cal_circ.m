f1ba0bc077f3cc3557b165fe0d7d4e8c46c032c0
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-17 22:24:36 -0300

    Add files via upload

---
+++ cal_circ.m
@@ -0,0 +1,9 @@
+clc
+clear all
+
+raio=input('digite o valor do raio: ');
+[area,perimetro]=circulo(raio);
+disp ('o valor da aera é: ')
+disp (area)
+disp ('o valor do periemetro é: ')
+disp (perimetro)
