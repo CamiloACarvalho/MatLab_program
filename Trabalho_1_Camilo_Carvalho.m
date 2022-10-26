4c334fb1098fc324ceb8a69961f1f67ec7b30607
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-10-26 18:42:27 -0300

    Add files via upload

---
+++ Trabalho_1_Camilo_Carvalho.m
@@ -0,0 +1,10 @@
+%Exercício para nota 01 - Homework
+%Camilo Augusto Carvalho 201071055
+%EPD061-B
+%28/08/2014
+h=15;
+r=8;
+V=pi*h*(r^2)
+%Após encontrar o valor do volume V, queremos encontar um novo raio para um
+%volume 20% maior, considere o novo raio como sendo R, assim temos:
+R=((1.2*V)/(h*pi))^0.5
