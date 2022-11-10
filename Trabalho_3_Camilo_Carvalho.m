fcadf4594aec2204cda2ef74367e3fead67a34b2
Camilo Augusto Carvalho <111397870+CamiloACarvalho@users.noreply.github.com>
2022-11-10 18:59:23 -0300

    Add files via upload

---
+++ Trabalho_3_Camilo_Carvalho.m
@@ -0,0 +1,32 @@
+%Trabalho para nota 2
+%Camilo Augusto Carvalho 201071055
+%EPD061-B
+%Resolver um problema de viagem  e comsumo de combustivel
+
+%Trabalho 3
+
+
+%Exercicio 1
+
+D=450;
+
+%Resposta da letra A
+l=D*12;
+
+%Resposta da letra B
+Cons=l*2.85;
+
+%Resposta da letra C
+Valor=Cons/(dist*9)-0.01;
+
+
+%Exercicio 2
+
+%Receber o custo de produção
+Custo=input('Digite o custo de produção do veículo');
+
+%Calculando o preço com o imposto
+Valor_1=Custo*(1+0.45);
+
+%preço com comissao distribuidor
+Valor_1=Valor_1*(1+0.28)
